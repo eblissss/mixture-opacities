c05f6b9adfc58bd8ede3a087aa18a67301880e99
Ethan Bliss <ebliss@stanford.edu>
2025-10-22 00:02:28 -0700

    fix: format and fix webdriver

--- README.md
+++ README.md
@@ -8,9 +8,8 @@ Our data source is https://aphysics2.lanl.gov/
 - Have Python Installed and use virtual environment
     - Run `python3 -m venv .venv`
 - Then install requirements
-    - Run `pip install selenium`
-- Download the [ChromeDriver](https://googlechromelabs.github.io/chrome-for-testing/)
-    - Add this to PATH
+    - Run `pip install selenium numpy webdriver-manager`
+- Maybe edit settings in `fetch_data.py`
 - Run `python fetch_data.py` to execute fetching
 - ???
 
--- fetch_data.py
+++ fetch_data.py
@@ -1,27 +1,33 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.select import Select
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
 import csv
 import os
 import numpy as np
 import time
 
-NUM_RUNS = 50
+NUM_RUNS = 1
 OUTPUT_FILE = "opacity_data.csv"
+USE_CHROME_DEV = True
+CHROME_VERSION = "143.0.7475.7"
+
 
 def fill_form(driver, mixture, temp_min, temp_max, rho_min, rho_max, num_rho):
     # Fill mixture field (e.g. 0.5 h 0.5 he)
     mixture_field = driver.find_element(By.ID, "user-spec-mix-value")
     mixture_field.clear()
     mixture_field.send_keys(mixture)
-    
+
     # Set temperature range (keV)
     tlow_select = Select(driver.find_element(By.ID, "temperature-low"))
-    tlow_select.select_by_value(temp_min)
+    tlow_select.select_by_visible_text(f" {temp_min}")
 
     tup_select = Select(driver.find_element(By.ID, "temperature-high"))
-    tup_select.select_by_value(temp_max)
-    
+    tup_select.select_by_visible_text(f" {temp_max}")
+
     # Set density range (g/cm^3)
     driver.find_element(By.NAME, "rlow").clear()
     driver.find_element(By.NAME, "rlow").send_keys(rho_min)
@@ -31,21 +37,22 @@ def fill_form(driver, mixture, temp_min, temp_max, rho_min, rho_max, num_rho):
 
     driver.find_element(By.NAME, "nr").clear()
     driver.find_element(By.NAME, "nr").send_keys(num_rho)
-    
+
     # Press submit
     driver.find_element(By.CSS_SELECTOR, "button[value='Submit']").click()
 
 
 def click_column_data(driver):
     driver.find_element(
-        By.XPATH, 
-        "//input[./*[contains(text(), 'Numerical by Columns')]]"
+        By.XPATH, "//input[./*[contains(text(), 'Numerical by Columns')]]"
     ).click()
 
 
 def extract_to_csv(driver, filename, mix_fractions):
     # Get the output text content
-    content = driver.find_element(By.CSS_SELECTOR, "code.text-muted").get_attribute("innerHTML")
+    content = driver.find_element(By.CSS_SELECTOR, "code.text-muted").get_attribute(
+        "innerHTML"
+    )
     lines = [line.strip() for line in content.split("<br>")]
 
     data = []
@@ -72,15 +79,35 @@ def extract_to_csv(driver, filename, mix_fractions):
             rosseland = parts[1]
             planck = parts[2]
 
-            data.append([mix_fractions[0], mix_fractions[1], mix_fractions[2], temp, density, rosseland, planck])
+            data.append(
+                [
+                    mix_fractions[0],
+                    mix_fractions[1],
+                    mix_fractions[2],
+                    temp,
+                    density,
+                    rosseland,
+                    planck,
+                ]
+            )
 
     # Update CSV file
-    mode = 'a' if os.path.exists(filename) else 'w'
-    with open(filename, mode, newline='') as file:
+    mode = "a" if os.path.exists(filename) else "w"
+    with open(filename, mode, newline="") as file:
         writer = csv.writer(file)
-        if mode == 'w':
+        if mode == "w":
             # Set CSV headers
-            writer.writerow(["Mix_H", "Mix_He", "Mix_Al", "Temperature", "Density", "Rosseland_opacity", "Planck_opacity"])
+            writer.writerow(
+                [
+                    "Mix_H",
+                    "Mix_He",
+                    "Mix_Al",
+                    "Temperature",
+                    "Density",
+                    "Rosseland_opacity",
+                    "Planck_opacity",
+                ]
+            )
         writer.writerows(data)
 
     return len(data)
@@ -89,12 +116,22 @@ def extract_to_csv(driver, filename, mix_fractions):
 def main():
     print(f"=== Fetching Data from LANL TOPS ===")
 
-    if (os.path.exists(OUTPUT_FILE)):
+    if os.path.exists(OUTPUT_FILE):
         os.remove(OUTPUT_FILE)
-    
+
     # Start browser
-    driver = webdriver.Chrome()
-    
+
+    chrome_options = Options()
+    if USE_CHROME_DEV:
+        chrome_options.binary_location = (
+            "/Applications/Google Chrome Dev.app/Contents/MacOS/Google Chrome Dev"
+        )
+
+    driver = webdriver.Chrome(
+        service=Service(ChromeDriverManager(driver_version=CHROME_VERSION).install()),
+        options=chrome_options,
+    )
+
     try:
         for run in range(1, NUM_RUNS + 1):
             print(f"\n Fetching run {run}/{NUM_RUNS}")
@@ -102,15 +139,23 @@ def main():
             # Navigate to site
             print("\tNavigating to https://aphysics2.lanl.gov/")
             driver.get("https://aphysics2.lanl.gov/")
-            time.sleep(2)
+            time.sleep(3)
 
             # Get random element mixture
             fractions = np.random.dirichlet([1, 1, 1])
             mixture = f"{fractions[0]} h {fractions[1]} he {fractions[2]} al"
-            
-            # Page 1: Fill form
+
+            # Page 1: Fill form (make sure options are valid on site)
             print("\tFilling in form")
-            fill_form(driver, mixture, temp_min=2e-5, temp_max=50, rho_min=1e-5, rho_max=100, num_rho=20)
+            fill_form(
+                driver,
+                mixture,
+                temp_min="0.0005",
+                temp_max="40",
+                rho_min="0.00001",
+                rho_max="100.",
+                num_rho="20",
+            )
             print("\tForm submitted!")
             time.sleep(3)
 
@@ -118,22 +163,23 @@ def main():
             click_column_data(driver)
             print("\tColumn data selected")
             time.sleep(3)
-            
+
             # Page 3: Extract data
             print("\tExtracting column data")
             num_extracted = extract_to_csv(driver, OUTPUT_FILE, fractions)
             print(f"\tExtracted {num_extracted} data points!")
-            
+
             print(f"Fetch complete for mixture {mixture}")
-        
+
         print(f"All runs complete! Check {OUTPUT_FILE}")
-        
+
     except Exception as e:
         print(f"\nError (run {run}): {e}")
-        
+
     finally:
         time.sleep(2)
         driver.quit()
+
 
 if __name__ == "__main__":
     main()
